39e942cf4863e1a8bde2a5f72fedbe059b68ba0a
fems01 <2777270040@qq.com>
2023-03-30 20:36:22 +0800

    第一次测试提交

--- src/main/java/com/jmu/utim/common/CustomException.java
+++ src/main/java/com/jmu/utim/common/CustomException.java
@@ -3,6 +3,10 @@ package com.jmu.utim.common;
 /**
  * 自定义业务异常类
  */
+
+/**
+ *
+ */
 public class CustomException extends RuntimeException {
     public CustomException(String message){
         super(message);
